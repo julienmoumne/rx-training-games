5860101d5293184f00e0581ba022eaf7e212d3cd
Julien Moumné <julien@moumne.com>
2016-04-06 00:15:26 +0200

    use hotshell to publish gh-pages

--- hs.js
+++ hs.js
@@ -1,7 +1,21 @@
-item({desc: 'rx-training-games'}, function (){
-
+item({desc: 'rx-training-games'}, function () {
     item({key: 'i', cmd: 'sudo npm install'})
     item({key: 'd', desc: 'dev mode build & run', cmd: 'npm start'})
     item({key: 'p', desc: 'prod mode build & run', cmd: 'npm run start:prod'})
-    item({key: 'g', desc: 'generate gh-pages', cmd: './publish.sh'})
+    item({key: 'g', desc: 'generate gh-pages' + '\n  ', cmd: script(
+        'set -eu',
+        'git checkout gh-pages',
+        'git merge master -m "Merge branch \'master\' into gh-pages"',
+        'npm run build:prod',
+        'git add -A',
+        'git commit -m "publish rx-training-games"',
+        'git push',
+        'git checkout master'
+    )})
 })
+
+function script () {
+    src = '';
+    _.each(arguments, function (el, ix) { src += '   ' + el + '\n' })
+    return src
+}
--- publish.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-git checkout gh-pages
-git merge master -m "Merge branch 'master' into gh-pages"
-npm run build:prod
-git add -A
-git commit -m 'publish rx-training-games'
-git push
-git checkout master
